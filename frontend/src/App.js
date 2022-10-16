b81d144018bf99aa31969ae485da2728395eec43
jacksongski <jacksongski@icloud.com>
2022-10-16 14:06:04 -0500

    back end working, buggy front end

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,25 +1,60 @@
-import logo from './logo.svg';
-import './App.css';
+import "antd/dist/antd.css"
+
+import { CheckCircleOutlined, MinusCircleOutlined } from "@ant-design/icons"
+import { Col, Row, Timeline } from "antd"
+import { useEffect, useState } from "react"
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    const [tasks, setTasks] = useState([])
+    const [timeline, setTimeline] = useState([])
+
+    useEffect(() => {
+        const fetchAllTasks = async () => {
+            const response = await fetch("/task/")
+            const fetchedTasks = await response.json()
+            setTasks(fetchedTasks)
+        }
+
+        const interval = setInterval(fetchAllTasks, 1000)
+
+        return () => {
+            clearInterval(interval)
+        }
+    }, [])
+
+    useEffect(() => {
+        const timelineItems = tasks.reverse().map((task) => {
+            return task.completed ? (
+                <Timeline.Item
+                    dot={<CheckCircleOutlined />}
+                    color="green"
+                    style={{ textDecoration: "line-through", color: "green" }}
+                >
+                    {task.name} <small>({task._id})</small>
+                </Timeline.Item>
+            ) : (
+                <Timeline.Item
+                    dot={<MinusCircleOutlined />}
+                    color="blue"
+                    style={{ textDecoration: "initial" }}
+                >
+                    {task.name} <small>({task._id})</small>
+                </Timeline.Item>
+            )
+        })
+
+        setTimeline(timelineItems)
+    }, [tasks])
+
+    return (
+        <>
+            <Row style={{ marginTop: 50 }}>
+                <Col span={14} offset={5}>
+                    <Timeline mode="alternate">{timeline}</Timeline>
+                </Col>
+            </Row>
+        </>
+    )
 }
 
-export default App;
+export default App
