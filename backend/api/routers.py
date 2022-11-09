8490c5d8865ffd92bc5bdc463069ea091f350ac9
tyler7kehoe <kehoe.tyler@att.net>
2022-11-09 13:39:06 -0600

    Fix routers bug, await was in the wrong spot

--- backend/api/routers.py
+++ backend/api/routers.py
@@ -34,18 +34,18 @@ def get_api_router(app):
     @router.get("/users", response_description="List Users")
     async def list_users(request: Request):
         users = []
-        db = await request.app.mongodb["users"]
-        for doc in db.find().to_list(length=100):
+        db = request.app.mongodb["users"]
+        for doc in await db.find().to_list(length=100):
             users.append(doc)
         return JSONResponse(status_code=status.HTTP_200_OK, content=users)
 
     # This path allows to create a new user
     @router.post("/user", response_description="Add User")
     async def add_user(request: Request, user: UserModel = Body(...)):
-        db = await request.app.mongodb["users"]
+        db = request.app.mongodb["users"]
         user = jsonable_encoder(user)
-        new_user = db.insert_one(user)
-        created_user = db.find_one({"_id": new_user.inserted_id})
+        new_user = await db.insert_one(user)
+        created_user = await db.find_one({"_id": new_user.inserted_id})
         # Return a success created response
         return JSONResponse(status_code=status.HTTP_201_CREATED,
                             content=created_user)
@@ -53,8 +53,8 @@ def get_api_router(app):
     # This path allows to get a user
     @router.get("/user/{id}", response_description="Get User")
     async def get_user(id: str, request: Request):
-        db = await request.app.mongodb["users"]
-        if (user := db.find_one({"_id": id})) is not None:
+        db = request.app.mongodb["users"]
+        if (user := await db.find_one({"_id": id})) is not None:
             return JSONResponse(status_code=status.HTTP_201_CREATED,
                                 content=user)
         # Return an error if no user if found
@@ -63,19 +63,19 @@ def get_api_router(app):
     # This path allows to update a user
     @router.put("/user/{id}", response_description="Update User")
     async def update_user(id: str, request: Request, user: UserUpdateModel = Body(...)):
-        db = await request.app.mongodb["users"]
+        db = request.app.mongodb["users"]
     
         user = {k: v for k, v in user.dict().items() if v is not None}
 
         if len(user) >= 1:
-            update_result = db.update_one({"_id": id}, {"$set": user})
+            update_result = await db.update_one({"_id": id}, {"$set": user})
 
             if update_result.modified_count == 1:
-                if (updated_user := db.find_one({"_id": id})) is not None:
+                if (updated_user := await db.find_one({"_id": id})) is not None:
                     return JSONResponse(status_code=status.HTTP_201_CREATED,
                                         content=updated_user)
 
-        if (existing_user := db.find_one({"_id": id})) is not None:
+        if (existing_user := await db.find_one({"_id": id})) is not None:
             return JSONResponse(status_code=status.HTTP_201_CREATED,
                                 content=existing_user)
 
@@ -85,9 +85,9 @@ def get_api_router(app):
     # This path allows to delete a user
     @router.delete("/user/{id}", response_description="Delete User")
     async def delete_user(id: str, request: Request):
-        db = await request.app.mongodb["users"]
+        db = request.app.mongodb["users"]
 
-        delete_result = db.delete_one({"_id": id})
+        delete_result = await db.delete_one({"_id": id})
 
         if delete_result.deleted_count == 1:
             return JSONResponse(status_code=status.HTTP_204_NO_CONTENT)
