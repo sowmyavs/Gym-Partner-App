37f30a29b4daac5ca5abd485872660915426fa50
tyler7kehoe <kehoe.tyler@att.net>
2022-11-09 21:18:12 -0600

    naming changes

--- backend/api/image_storage.py
+++ backend/api/image_storage.py
@@ -3,26 +3,26 @@ from google.cloud import storage
 from io import BytesIO
 import os
 
-gcp_bucket_name="bigbuffbadgers"
-gcp_key='key.json'
-gcp_project_name="BigBuffBadgers"
+GCP_BUCKET_NAME = "bigbuffbadgers"
+GCP_KEY = 'key.json'
+GCP_PROJECT_NAME = "BigBuffBadgers"
 
 class ImageManager:
     def store_image_gcp(gcp_image_name,image_path="../profile_image.jpg"):
         # get GCP key
         dir_path = os.path.dirname(os.path.realpath(__file__))
-        gcp_key1 = os.path.join(dir_path, gcp_key)
+        gcp_key1 = os.path.join(dir_path, GCP_KEY)
         
         # Connect to GCP client
-        storage_client = storage.Client.from_service_account_json(gcp_key1, project=gcp_project_name)
-        bucket = storage_client.get_bucket(gcp_bucket_name)
+        storage_client = storage.Client.from_service_account_json(gcp_key1, project=GCP_PROJECT_NAME ) 
+        bucket = storage_client.get_bucket(GCP_BUCKET_NAME)
         path = os.path.join(dir_path, image_path)
         
         # Create blob for file upload
         filename = "%s%s" % ('',path)
         blob = bucket.blob("profileimages/{}.jpg".format(gcp_image_name))
         blob.content_type = "image/jpeg"
-        gcp_image_path=gcp_bucket_name+'/'+'profileimages'+'/'+gcp_image_name+'.jpg'
+        gcp_image_path=GCP_BUCKET_NAME+'/'+'profileimages'+'/'+gcp_image_name+'.jpg'
         
         # Upload image to GCP
         with open(path, 'rb') as f:
@@ -35,6 +35,6 @@ class ImageManager:
     # Returns a link to a specific GCP image
     def get_link(file_name):
         API_ENDPOINT = 'https://storage.googleapis.com'
-        image_link = API_ENDPOINT + f"/{gcp_bucket_name}/profileimages/{file_name}.jpg"
+        image_link = API_ENDPOINT + f"/{GCP_BUCKET_NAME}/profileimages/{file_name}.jpg"
         return image_link
         
